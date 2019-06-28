ebd262ec675aebdc25bec2ad45d9e9ecff1dd899
David Adewoyin <dadewoyin16@gmail.com>
2019-06-28 12:00:26 -0400

    possible day 1 code?

--- components/CreatePost.js
+++ components/CreatePost.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { Modal, TextInput, View, TouchableHighlight, Text } from 'react-native';
+import { Modal, TextInput, View, TouchableHighlight, Image, StyleSheet, Button } from 'react-native';
 
 export class CreatePost extends Component {
   constructor(props) {
     super(props)
-    console.log('props: ', props)
     this.state = {
       text: ''
     }
+  }
+
+  createPost() {
+    console.log('ayoooo', this.state.text)
+    // make call to Firebase
   }
 
   render() {
@@ -17,22 +21,43 @@ export class CreatePost extends Component {
         transparent={false}
         visible={this.props.isModalVisible}
       >
-        <View style={{ marginTop: 22 }}>
+        <View style={{ marginTop: 25 }}>
+          <TouchableHighlight
+            onPress={() => {
+              this.props.closeModal()
+            }}>
+            <Image 
+              source={{ uri: 'https://cdn4.iconfinder.com/data/icons/media-controls-4/100/close-512.png' }} 
+              style={styles.close}
+            />
+          </TouchableHighlight>
+          
           <TextInput
             multiline={true}
             numberOfLines={4}
             onChangeText={(text) => this.setState({ text })}
             placeholder="Tell your friends something here..."
-            value={""} />
+            value={this.state.text} 
+            style={styles.input}
+          />
 
-          <TouchableHighlight
-            onPress={() => {
-              this.props.closeModal()
-            }}>
-            <Text>Close Modal</Text>
-          </TouchableHighlight>
         </View>
+
+        <Button title="Create Post" onPress={() => this.createPost()}/>
       </Modal>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 80
+  },
+  close: {
+    width: 40,
+    height: 40,
+    alignSelf: 'flex-end',
+    marginRight: 10,
+    marginBottom: 10
+  }
+})
